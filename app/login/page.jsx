7356c85bdbcef6bd200c5f06a2d5ef961b12e3a1
Stephen Monroe <stephen@mymonroe.net>
2025-01-22 06:31:29 -0600

    Starting login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,35 @@
 const LoginPage = () => {
+
+	const login = () => {
+		console.log("login");
+	}
+
+	const signup = () => {
+		console.log("signup");
+	}
+
 	return (
+		<>
 
-		<div>Login</div>
+		<div className="container mx-auto">
+			<form className="bg-gray-100 my-10">
+				<div className="mb-4">
+					<label className="block" htmlFor="email">Email</label>
+					<input className="block w-full bg-gray-200 text-sm rounded-lg" id="email" name="email" type="email" required />
+				</div>
+				
 
-	)
-}
+				<label htmlFor="password">Password</label>
+				<input id="password" name="password" type="password" required />
+
+				<button formAction={login()}>Log in</button>
+				<button formAction={signup()}>Sign up</button>
+			</form>
+		</div>
+
+</>
+	);
+
+};
 
 export default LoginPage;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ export default {
 			fontFamily: {
 				sans: ["Poppins", "sans-serif"]
 			}
-		},
+		}
 	},
 	plugins: [],
 };
